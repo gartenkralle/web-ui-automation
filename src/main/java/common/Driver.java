d215131d50aaf9d9624f2f6730076f96a282c963
Clemens Schneider <clemens.schneider@posteo.de>
2018-05-04 09:11:34 +0200

    restrict access

--- src/main/java/common/Driver.java
+++ src/main/java/common/Driver.java
@@ -3,7 +3,7 @@ package common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public final class Driver
+final class Driver
 {
     private static WebDriver driver;
     
