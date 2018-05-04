d11a9ed63d7ee5de4c36b0c94502349759064b97
Clemens Schneider <clemens.schneider@posteo.de>
2018-05-04 09:20:31 +0200

    removed local exception handling

--- src/main/java/common/UserInterface.java
+++ src/main/java/common/UserInterface.java
@@ -3,10 +3,8 @@ package common;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -97,14 +95,7 @@ public final class UserInterface
         
         public static void visitUrl(String url)
         {
-            try
-            {
-                driver.get(url);
-            }
-            catch(TimeoutException ex)
-            {
-                Assert.fail();
-            }
+            driver.get(url);
         }
     }
     
