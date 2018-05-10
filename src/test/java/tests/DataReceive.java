5a0194207b26fa5527e60e11765b5311904748bd
Clemens Schneider <clemens.schneider@posteo.de>
2018-05-10 17:22:23 +0200

    add unit test

--- src/test/java/tests/DataReceive.java
+++ src/test/java/tests/DataReceive.java
@@ -11,8 +11,10 @@ import common.UserInterface;
 public class DataReceive extends TestBase
 {
     private final static String TABLE_URL = "https://www.w3schools.com/html/html_tables.asp";
+    private final static String INNER_TEXT_URL = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_innertext";
     
     private final static By TABLE = By.xpath("//*[@id='customers']");
+    private final static By TRY_IT_BUTTON = By.xpath("//*[@id='myBtn']");
     
     @Test
     public void getTable1()
@@ -89,5 +91,16 @@ public class DataReceive extends TestBase
         
         UserInterface.Verify.equals(1, count);
         
+    }
+    
+    @Test
+    public void getText()
+    {
+        UserInterface.Action.visitUrl(INNER_TEXT_URL);
+        UserInterface.Action.selectFrame("iframeResult");
+        
+        String buttonText = UserInterface.DataReceive.getText(TRY_IT_BUTTON);
+        
+        UserInterface.Verify.equals("Try it", buttonText);
     }
 }
