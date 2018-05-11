139e3209ece951e700908b5e793bd95e2e7b5dda
Clemens Schneider <clemens.schneider@posteo.de>
2018-05-12 01:29:56 +0200

    simplify table creation

--- src/main/java/common/Table.java
+++ src/main/java/common/Table.java
@@ -24,6 +24,18 @@ public class Table implements Iterable<Table.Row>
         return rows.get(index);
     }
     
+    public static Table create(Row... rows)
+    {
+        Table table = new Table();
+        
+        for(Row row : rows)
+        {
+            table.add(row);
+        }
+        
+        return table;
+    }
+    
     public static class Row
     {
         private final List<String> cols = new ArrayList<>();
@@ -36,6 +48,18 @@ public class Table implements Iterable<Table.Row>
         public String get(int index)
         {
             return cols.get(index);
+        }
+        
+        public static Row create(String... cols)
+        {
+            Row row = new Row();
+            
+            for(String col : cols)
+            {
+                row.add(col);
+            }
+            
+            return row;
         }
     }
 }
--- src/test/java/tests/DataReceive.java
+++ src/test/java/tests/DataReceive.java
@@ -21,52 +21,15 @@ public class DataReceive extends TestBase
     {
         UserInterface.Action.visitUrl(TABLE_URL);
         
+        Table expectedTable = Table.create(Row.create("Company", "Contact", "Country"),
+                                           Row.create("Alfreds Futterkiste", "Maria Anders", "Germany"),
+                                           Row.create("Centro comercial Moctezuma", "Francisco Chang", "Mexico"),
+                                           Row.create("Ernst Handel", "Roland Mendel", "Austria"),
+                                           Row.create("Island Trading", "Helen Bennett", "UK"),
+                                           Row.create("Laughing Bacchus Winecellars", "Yoshi Tannamuri", "Canada"),
+                                           Row.create("Magazzini Alimentari Riuniti", "Giovanni Rovelli", "Italy"));
+        
         Table actualTable = UserInterface.DataReceive.getTable(TABLE);
-        
-        Table expectedTable = new Table();
-        Row row;
-        
-        row = new Row();
-        row.add("Company");
-        row.add("Contact");
-        row.add("Country");
-        expectedTable.add(row);
-        
-        row = new Row();
-        row.add("Alfreds Futterkiste");
-        row.add("Maria Anders");
-        row.add("Germany");
-        expectedTable.add(row);
-        
-        row = new Row();
-        row.add("Centro comercial Moctezuma");
-        row.add("Francisco Chang");
-        row.add("Mexico");
-        expectedTable.add(row);
-        
-        row = new Row();
-        row.add("Ernst Handel");
-        row.add("Roland Mendel");
-        row.add("Austria");
-        expectedTable.add(row);
-        
-        row = new Row();
-        row.add("Island Trading");
-        row.add("Helen Bennett");
-        row.add("UK");
-        expectedTable.add(row);
-        
-        row = new Row();
-        row.add("Laughing Bacchus Winecellars");
-        row.add("Yoshi Tannamuri");
-        row.add("Canada");
-        expectedTable.add(row);
-        
-        row = new Row();
-        row.add("Magazzini Alimentari Riuniti");
-        row.add("Giovanni Rovelli");
-        row.add("Italy");
-        expectedTable.add(row);
         
         UserInterface.Verify.equals(expectedTable, actualTable);
     }
