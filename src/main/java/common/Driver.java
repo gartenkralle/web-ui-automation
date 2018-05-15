398e50cc44870dbd4869a70518d9d0f22af97250
Clemens Schneider <clemens.schneider@posteo.de>
2018-05-15 20:49:44 +0200

    jar handling

--- src/main/java/common/Driver.java
+++ src/main/java/common/Driver.java
@@ -1,5 +1,13 @@
 package common;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,7 +22,7 @@ final class Driver
     
     public static void setup()
     {
-        Driver.instance = new ChromeDriver();
+        Driver.instance = getChromeDriver();
         
         Common.Setting.setup(Driver.instance);
     }
@@ -22,5 +30,43 @@ final class Driver
     public static void quit()
     {
         Driver.instance.quit();
+    }
+    
+    private static ChromeDriver getChromeDriver()
+    {
+        String chromedriver = "chromedriver.exe";
+        final String resource = Driver.class.getClassLoader().getResource(chromedriver).getFile();
+        
+        if(new File(resource).isFile())
+        {
+            chromedriver = resource;
+        }
+        else if(!new File(chromedriver).isFile())
+        {
+            try
+            {
+                Path temp = Files.createTempFile("temp", ".tmp");
+                Files.copy(Driver.class.getClassLoader().getResourceAsStream(chromedriver), temp, StandardCopyOption.REPLACE_EXISTING);
+                FileInputStream inputStream = new FileInputStream(temp.toFile());
+                
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                
+                File file = new File(chromedriver);
+                OutputStream outputStream = new FileOutputStream(file);
+                outputStream.write(buffer);
+                outputStream.flush();
+                outputStream.close();
+                inputStream.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        
+        System.setProperty("webdriver.chrome.driver", chromedriver);
+        
+        return new ChromeDriver();
     }
 }
