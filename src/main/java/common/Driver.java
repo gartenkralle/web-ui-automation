e0046ff7c38d4236819b8d349142c93ec60f1b76
Clemens Schneider <clemens.schneider@posteo.de>
2018-05-16 17:07:27 +0200

    linux support

--- src/main/java/common/Driver.java
+++ src/main/java/common/Driver.java
@@ -3,11 +3,16 @@ package common;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.apache.commons.lang.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -34,7 +39,32 @@ final class Driver
     
     private static ChromeDriver getChromeDriver()
     {
-        String chromedriver = "chromedriver.exe";
+        ChromeDriver chromeDriver =  null;
+        
+        if(SystemUtils.IS_OS_LINUX)
+        {
+            chromeDriver = getLinuxChromeDriver();
+        }
+        else if(SystemUtils.IS_OS_WINDOWS)
+        {
+            chromeDriver = getWindowsChromeDriver();
+        }
+        
+        return chromeDriver;
+    }
+    
+    private static ChromeDriver getLinuxChromeDriver()
+    {
+        return getChromeDriver("chromedriver");
+    }
+    
+    private static ChromeDriver getWindowsChromeDriver()
+    {
+        return getChromeDriver("chromedriver.exe");
+    }
+    
+    private static ChromeDriver getChromeDriver(String chromedriver)
+    {
         final String resource = Driver.class.getClassLoader().getResource(chromedriver).getFile();
         
         if(new File(resource).isFile())
@@ -43,30 +73,60 @@ final class Driver
         }
         else if(!new File(chromedriver).isFile())
         {
-            try
-            {
-                Path temp = Files.createTempFile("temp", ".tmp");
-                Files.copy(Driver.class.getClassLoader().getResourceAsStream(chromedriver), temp, StandardCopyOption.REPLACE_EXISTING);
-                FileInputStream inputStream = new FileInputStream(temp.toFile());
-                
-                byte[] buffer = new byte[inputStream.available()];
-                inputStream.read(buffer);
-                
-                File file = new File(chromedriver);
-                OutputStream outputStream = new FileOutputStream(file);
-                outputStream.write(buffer);
-                outputStream.flush();
-                outputStream.close();
-                inputStream.close();
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
+            CopyResourceFileToChildProject(chromedriver);
+        }
+        
+        if(SystemUtils.IS_OS_LINUX)
+        {
+            setExecutePermission(chromedriver);
         }
         
         System.setProperty("webdriver.chrome.driver", chromedriver);
         
         return new ChromeDriver();
+    }
+    
+    private static void setExecutePermission(String driver)
+    {
+        setExecutePermission(new File(driver));
+    }
+    
+    private static void setExecutePermission(File file)
+    {
+        Set<PosixFilePermission> perms = new HashSet<>();
+        perms.add(PosixFilePermission.OWNER_EXECUTE);
+        
+        try
+        {
+            Files.setPosixFilePermissions(file.toPath(), perms);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void CopyResourceFileToChildProject(String filename)
+    {
+        try
+        {
+            Path temp = Files.createTempFile("temp", ".tmp");
+            Files.copy(Driver.class.getClassLoader().getResourceAsStream(filename), temp, StandardCopyOption.REPLACE_EXISTING);
+            FileInputStream inputStream = new FileInputStream(temp.toFile());
+            
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            
+            File file = new File(filename);
+            OutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(buffer);
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
